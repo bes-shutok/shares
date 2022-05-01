388de958ea2fc98864296c16831819bd6c7aeaf7
Andrey Dmitriev <dmitriev.andrey.vitalyevich@gmail.com>
2022-05-01 11:28:09 +0100

    Using intermediary trade action object

--- reporting.py
+++ reporting.py
@@ -11,10 +11,11 @@ class TradeType(Enum):
 
 
 class TradeAction:
-    def __init__(self, symbol, date, currency, quantity, price, fee):
+    def __init__(self, symbol, date_time, currency, quantity, price, fee):
+        # self.keys = ['time', 'Quantity', 'Price', 'Fee']
         self.symbol = symbol
-        print(datetime.strptime(date, '%Y-%m-%d, %H:%M:%S'))
-        self.date = datetime.strptime(date, '%Y-%m-%d, %H:%M:%S')
+        print(datetime.strptime(date_time, '%Y-%m-%d, %H:%M:%S'))
+        self.date_time = datetime.strptime(date_time, '%Y-%m-%d, %H:%M:%S')
         self.currency = currency
         if int(quantity) < 0:
             self.type = TradeType.SELL
@@ -24,15 +25,20 @@ class TradeAction:
             self.quantity = int(quantity)
 
         self.price = price
-        self.fee = fee
+        self.fee = str(fee).lstrip("-")
+
+
+    @staticmethod
+    def keys():
+        return ['time', 'Quantity', 'Price', 'Fee']
 
     def print(self):
         if self.type == TradeType.BUY:
             print("Bought " + str(self.quantity) + " " + self.symbol + " shares" + " for " +
-                  self.price + " " + self.currency + " at " + str(self.date))
+                  self.price + " " + self.currency + " at " + str(self.date_time))
         else:
             print("Sold " + str(self.quantity) + " " + self.symbol + " shares" + " for " +
-                  self.price + " " + self.currency + " at " + str(self.date))
+                  self.price + " " + self.currency + " at " + str(self.date_time))
 
 
 print("This line will be printed.")
@@ -44,24 +50,35 @@ result_file = 'trades.csv'
 result_path = os.path.join(work_dir, result_file)
 print(result_path)
 
+trade_actions = {}
+share_trade_actions = []
 trades = {}
 share_trades = []
 # open file in read mode
 with open(source_path, 'r') as read_obj:
     csv_dict_reader = csv.DictReader(read_obj)
     for row in csv_dict_reader:
-        key = row['Symbol']
-        if key in trades.keys():
-            share_trades = trades[row['Symbol']]
-        share_trades.append(
-            {'time': row['Date/Time'], 'Quantity': row['Quantity'], 'Price': row['T. Price'], 'Fee': row['Comm/Fee']})
         if row['Date/Time'] != "":
-            t = TradeAction(row['Symbol'], row['Date/Time'], row['Currency'], row['Quantity'], row['T. Price'], row['Comm/Fee'])
-            t.print()
-        trades[row['Symbol']] = share_trades
-        print(row['Currency'], row['Symbol'], row['Date/Time'], row['Quantity'], row['T. Price'], row['Comm/Fee'])
+            key = row['Symbol']
+            if key in trade_actions.keys():
+                share_trade_actions = trade_actions[key]
 
-print(trades)
+            print(row['Currency'], row['Symbol'], row['Date/Time'], row['Quantity'], row['T. Price'], row['Comm/Fee'])
+            t = TradeAction(row['Symbol'], row['Date/Time'], row['Currency'], row['Quantity'], row['T. Price'],
+                            row['Comm/Fee'])
+            t.print()
+            share_trade_actions.append(t)
+            trade_actions[key] = share_trade_actions
+
+print(trade_actions)
+
+for k, v in trade_actions.items():
+    actions = trade_actions[k]
+    share_trades = []
+    for action in actions:
+        share_trades.append({'time': action.date_time.date(), 'Quantity': str(action.quantity),
+                             'Price': action.price, 'Fee': action.fee})
+    trades[k] = share_trades
 
 with open(result_path, "w+", newline='', encoding='utf-8') as csv_file:
     csv_columns = ['time', 'Quantity', 'Price', 'Fee']
