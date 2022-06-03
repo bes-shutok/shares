4a43fcd3723d9f99ba3cf0756fb1e8e9ba9aa056
Andrey Dmitriev <dmitriev.andrey.vitalyevich@gmail.com>
2022-06-03 08:56:42 +0100

    tests cleanup

--- parsing.py
+++ parsing.py
@@ -36,20 +36,3 @@ def parse_data(path: Union[str, Path[str]]) -> TradeActionsPerCompany:
                 trade_actions_per_company[currency_company] = trade_actions
 
     return trade_actions_per_company
-
-
-# Doesn't seem valuable - too much information is lost while converting to xlsx.
-# Let's just create test object
-def parse_results(path: Union[str, PathLike[str]]):
-    xlsx_file = Path('resources', 'capital_gains.xlsx')
-    print("This line will be printed.")
-    print(path)
-
-    wb_obj = openpyxl.load_workbook(xlsx_file)
-    # Read the active sheet:
-    sheet = wb_obj.active
-    col_names = []
-    for column in sheet.iter_cols(1, sheet.max_column):
-        col_names.append(column[0].value)
-
-    print(col_names)
--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -2,7 +2,7 @@ import unittest
 from pathlib import Path
 
 import parsing
-from supplementary import TradeActionsPerCompany, TradeType
+from supplementary import TradeType
 from test_common_data import simple_trade, test_symbol
 
 
@@ -15,14 +15,6 @@ class MyTestCase(unittest.TestCase):
         self.assertEqual(simple_trade[test_symbol][TradeType.SELL][1], actual_trades[test_symbol][TradeType.SELL][1])
         self.assertEqual(simple_trade[test_symbol][TradeType.SELL], actual_trades[test_symbol][TradeType.SELL])
         self.assertEqual(simple_trade, actual_trades)
-
-    @staticmethod
-    def test_capital_gain_lines():
-        source_file = Path('../resources', 'simple.csv')
-        actual_trades: TradeActionsPerCompany = parsing.parse_data(source_file)
-        xlsx_file = Path('../resources', 'capital_gains.xlsx')
-        actual_trades: TradeActionsPerCompany = parsing.parse_results(xlsx_file)
-        results_source_file = "resources/capital_gains.csv"
 
 
 if __name__ == '__main__':
